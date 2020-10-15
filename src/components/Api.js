c9806cfed6b8f3e757465b57007f069c3eb7fef1
rahuljha661 <rahuljha12527@gmail.com>
2020-10-15 15:59:07 +0530

    create a ui and install store

---
+++ src/components/Api.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Api=()=>{ 
+    return (
+        <div>
+            <input type="text" placeholder="Enter Github UserName."/>
+            <br/>
+            <button>Search</button>
+        </div>
+    )
+}
+
+export default Api;
---
+++ src/store/reducers/reducer.js
@@ -0,0 +1,8 @@
+const initState={};
+  
+const reducer=(state=initState,action)=>{
+   return state;
+}
+
+
+export default reducer;
